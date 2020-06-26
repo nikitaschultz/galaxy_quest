01b13a2f338e4b3d6b78e973b4e1210ea3a0cb45
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-06-26 13:41:08 +0100

    server and router set up

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('galaxy_quest');
+    const profilesCollection = db.collection('profiles');
+    const profilesRouter = createRouter(profilesCollection);
+    app.use('/api/profiles', profilesRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
+})
