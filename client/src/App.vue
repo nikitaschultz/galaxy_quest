ed1a5ac56beaa66a2d5a3b00b7aef46d3f253ea4
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-06-26 14:01:39 +0100

    basic app set up

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,22 @@
 <template lang="html">
-
+  <div class="">
+    <profile-container v-if="profileView = true" />
+  </div>
 </template>
 
 <script>
+import ProfileContainer from './components/Profiles/ProfileContainer.vue';
+
 export default {
+  name: "app",
+  components: {
+    "profile-container": ProfileContainer
+  },
+  data(){
+    return {
+      profileView: true
+    }
+  }
 }
 </script>
 
